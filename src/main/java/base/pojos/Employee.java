7e546f5ae0a764f17facbb81affd61fe0b5c42bc
JMan <60026263+JMantis0@users.noreply.github.com>
2021-05-01 18:49:55 -0700

    Added getters, setters, constructors to Employee class

--- src/main/java/base/pojos/Employee.java
+++ src/main/java/base/pojos/Employee.java
@@ -1,8 +1,66 @@
 package base.pojos;
 
+import org.bson.types.ObjectId;
+
+
+
 public class Employee {
   String firstName;
   String lastName;
   int empID;
   String password;
+  ObjectId _id;
+
+  //Constructors
+  public Employee() {
+  }
+
+  public Employee(String firstName, String lastName, int empID, String password, ObjectId _id) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.empID = empID;
+    this.password = password;
+    this._id = _id;
+  }
+
+  //  S/G etters
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public int getEmpID() {
+    return empID;
+  }
+
+  public void setEmpID(int empID) {
+    this.empID = empID;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public ObjectId get_id() {
+    return _id;
+  }
+
+  public void set_id(ObjectId _id) {
+    this._id = _id;
+  }
 }
